8cdf0a379c11593081d9692663b03b84cc8ac468
Miro8D <miro8d@gmail.com>
2022-03-11 19:44:52 +0200

    simple hello world

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	return nil
+}
+
+func main() {
+	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+		log.Fatal(err)
+	}
+}
