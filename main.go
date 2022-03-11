a8c8154dee6f30f59f79b3e5c6975950fe4a1d25
Miro8D <miro8d@gmail.com>
2022-03-11 20:37:51 +0200

    two images drew and a simple text

--- main.go
+++ main.go
@@ -7,16 +7,35 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-func update(screen *ebiten.Image) error {
+var (
+	err 				error
+	background 	*ebiten.Image
+	pa 					*ebiten.Image
+)
+
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	pa, _, err = ebitenutil.NewImageFromFile("assets/pa.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func update (screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+	screen.DrawImage(background, op)
+	op.GeoM.Translate(0, 50)
+	background.DrawImage(pa, op)
+	ebitenutil.DebugPrint(pa, "Heyya")
 	return nil
 }
 
+
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "My Game!"); err != nil {
 		log.Fatal(err)
 	}
 }
