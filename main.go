eae087ec1e1c51a1350fc28f90467433702f8f4a
Miro8D <miro8d@gmail.com>
2022-03-11 21:35:13 +0200

    simple Rocket movements

--- main.go
+++ main.go
@@ -7,35 +7,79 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-var (
-	err 				error
-	background 	*ebiten.Image
-	pa 					*ebiten.Image
+const (
+	screenWidth, screenHeight = 640, 480
 )
 
-func init() {
-	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
-	pa, _, err = ebitenutil.NewImageFromFile("assets/pa.png", ebiten.FilterDefault)
+// Create the player class
+type player struct {
+	image				*ebiten.Image
+	xpos, ypos 	float64
+	speed 			float64
+}
+
+// vars
+var (
+	err					error
+	background 	*ebiten.Image
+	spaceShip		*ebiten.Image
+	playerOne		player
+)
+
+// Error Check
+func checkForErr() {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// This code will be ran once at startup
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	checkForErr()
+	spaceShip, _, err = ebitenutil.NewImageFromFile("assets/spaceShip.png", ebiten.FilterDefault)
+	checkForErr()
+	playerOne = player{spaceShip, screenWidth/2.0, screenHeight/2.0, 4}
+}
+// Keyboard events and movements
+func movePlayer(x float64, y float64){
+	if ebiten.IsKeyPressed(ebiten.KeyUp){
+		playerOne.ypos -= playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown){
+		playerOne.ypos += playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft){
+		playerOne.xpos -= playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight){
+		playerOne.xpos += playerOne.speed
+	}
+}
+
+// Update
 func update (screen *ebiten.Image) error {
+	movePlayer(playerOne.xpos, playerOne.ypos)
+
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(0, 0)
-	screen.DrawImage(background, op)
-	op.GeoM.Translate(0, 50)
-	background.DrawImage(pa, op)
-	ebitenutil.DebugPrint(pa, "Heyya")
+	// Draw our background
+	bgOp := &ebiten.DrawImageOptions{}
+	bgOp.GeoM.Translate(0, 0)
+	screen.DrawImage(background, bgOp)
+	// Draw our player
+	playerOp := &ebiten.DrawImageOptions{}
+	playerOp.GeoM.Scale(0.1, 0.1)
+	playerOp.GeoM.Translate(playerOne.xpos, playerOne.ypos)
+	screen.DrawImage(playerOne.image, playerOp)
+
 	return nil
 }
 
-
+// Main Loop
 func main() {
-	if err := ebiten.Run(update, 640, 480, 1, "My Game!"); err != nil {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "My Game!"); err != nil {
 		log.Fatal(err)
 	}
 }
